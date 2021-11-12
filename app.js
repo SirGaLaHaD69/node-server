4bcf3f8ea9475a4d5d900ca67e6481cf80770845
SirGaLaHaD69 <ritikparida@gmail.com>
2021-11-12 10:34:03 +0530

    added hi

--- app.js
+++ app.js
@@ -14,8 +14,9 @@ app.use('/uploads',express.static('uploads'))
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 app.get('/hello',(req,res)=>{
-    res.send("HELLO FROM AWS")
+    res.send("HELLO FROM AWS, HIIII")
 })
+
 app.use('/products',productRouter);
 app.use('/orders',orderRouter);
 app.use('/users',userRouter);
